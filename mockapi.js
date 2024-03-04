953cc6e8abb3eeaeada902a22796904831a38066
Ranjitha-1228 <ranjithamurugasan1228@gmail.com>
2024-03-04 11:33:26 +0530

    uodate

--- mockapi.js
+++ mockapi.js
@@ -1,16 +1,16 @@
 function getPostDataAndFilterByTitle(titleToFilter) {
-    // Assuming your mock API endpoint is 'https://mockapi.com/posts'
+   
     fetch('https://jsonplaceholder.typicode.com/posts')
         .then(response => response.json())
         .then(data => {
-            // Filter posts based on the title
+            
             const filteredPosts = data.filter(post => post.title.includes(titleToFilter));
             
-            // Log the filtered posts to console
+            
             console.log(filteredPosts);
         })
         .catch(error => console.error('Error fetching data:', error));
 }
 
-// Call the function with the title you want to filter
+
 getPostDataAndFilterByTitle('quam voluptatibus rerum veritatis');
